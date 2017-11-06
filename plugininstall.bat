b46fb2d6a17d4be0ee6a7823453ebebb2e93abe7
Mitchell Paulus <paulusm14@gmail.com>
2017-11-06 10:51:28 -0600

    Add batch script for starting up the personal plugin files on Windows

---
+++ plugininstall.bat
@@ -0,0 +1,6 @@
+mkdir %HOME%\pack\mitchplugins\start
+cd %HOME%\pack\mitchplugins\start
+git clone https://github.com/mitchpaulus/vim-siemens-ppcl.git
+git clone https://github.com/mitchpaulus/autocorrect.vim.git
+git clone https://github.com/mitchpaulus/vim-tex2text.git
+git clone https://github.com/mitchpaulus/latex-plus.git
