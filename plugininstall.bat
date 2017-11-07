1430e2948a0029ce397a7f3a6c70b1b37685358a
Mitchell Paulus <paulusm14@gmail.com>
2017-11-07 10:49:39 -0600

    Update install scripts to add Vundle.vim

--- plugininstall.bat
+++ plugininstall.bat
@@ -1,6 +1,16 @@
-mkdir %HOME%\pack\mitchplugins\start
-cd %HOME%\pack\mitchplugins\start
+mkdir %HOME%\vimfiles\pack\mitchplugins\start
+cd %HOME%\vimfiles\pack\mitchplugins\start
 git clone https://github.com/mitchpaulus/vim-siemens-ppcl.git
 git clone https://github.com/mitchpaulus/autocorrect.vim.git
 git clone https://github.com/mitchpaulus/vim-tex2text.git
 git clone https://github.com/mitchpaulus/latex-plus.git
+
+IF EXIST %HOME%\vimfiles\bundle\Vundle.vim (
+        echo "Vundle already exists"
+        )
+ELSE (
+        mkdir %HOME%\vimfiles\bundle
+        cd %HOME%\vimfiles\bundle
+        git clone https://github.com/VundleVim/Vundle.vim.git
+     )
+
